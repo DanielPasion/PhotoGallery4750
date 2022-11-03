cf40f55e11c2e5074ef5d0754a8e47759b91882a
Daniel Pasion <dnpaxion@gmail.com>
2022-11-03 01:02:21 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=bf99e8e55201b6c7f1070b0405b80f73" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
